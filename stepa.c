2601de50c1aa040606a6524b30e3c2a7f34bbe87
Степан <bokarevstepan1@yandex.ru>
2023-03-13 01:16:35 +0300

    добавил создание папок как до архивации

--- stepa.c
+++ stepa.c
@@ -4,10 +4,50 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #define LEN 200
-#define MAX_FILES_PATH_LEN 1000
+#define MAX_FILES_PATH_LEN 2000
 #define MAX_FILENAME_LEN 512
 #define MAX_LINE_LEN 1024
+
+/**
+ * Returns file permissions in mode_t format
+ * https://man7.org/linux/man-pages/man2/lstat.2.html
+*/
+int filePermissionsCStr(char const *path) {
+    struct stat stats;
+    if(stat(path, &stats) < 0) {
+        printf("filePermissions error: Cannot read file properties\n");
+        exit(1);
+    }
+    return stats.st_mode;
+}
+
+/**
+ * Overrides file permissions in mode_t format
+ * https://man7.org/linux/man-pages/man2/lstat.2.html
+*/
+void fileSetPermissionsCStr(char const *path, char permissions) {
+    if(chmod(path, permissions) != 0) {
+         printf("fileSetPermissionsCStr error: Cannot assign permission to selected file\n");
+        exit(1);
+        
+    }
+}
+/*int fileSetPermissionsCStr(const char *filename, const char *permissionCode) {
+    int result = -1;
+    mode_t mode = strtol(permissionCode, NULL, 8); // преобразуем строку в восьмеричное число
+
+    if (chmod(filename, mode) == 0) {
+        result = 0;
+    } else {
+        perror("fileSetPermissionsCStr");
+    }
+
+    return result;
+}*/
 
 // Функция, которая создает новый файл и записывает в него данные
 void create_new_file(char *filename, char *data) {
@@ -116,8 +156,6 @@ char **findTextFiles(const char *path, int *count) {
 
                 char *filename = concate_str(path, entry->d_name);
         
-               
-
                 files[i] = malloc(strlen(filename) + 1);
                 strcpy(files[i], filename);
                 free_concate_str(filename);
@@ -141,8 +179,9 @@ void mergeTextFiles(char **files, int count, const char *outputFile) {
 
     for (int i = 0; i < count; i++) {
         char *content = readFile(files[i]);
+        int permissionCode = filePermissionsCStr(files[i]);
         if (content) {
-            fprintf(fp, "<<<<%s\n%s>>>>\n", files[i], content);
+            fprintf(fp, "<<<<%s|||%d|||\n%s\n>>>>\n", files[i], permissionCode, content);
             free(content);
         }
     }
@@ -163,30 +202,101 @@ bool isCorrectNameFile(const char* filename){
     int suffix_len = strlen(suffix);
 
     if (str_len < suffix_len || strcmp(filename + str_len - suffix_len, suffix) != 0) {
-        printf("Строка не заканчивается на .txt\n");
-        printf("Укажите путь к архиву с расширением .txt\n");
         return false;
-        //
     } 
 
     return true;
 
 }
+// Функция для проверки существования директории по переданному пути
+bool directoryExists(const char *path) {
+    struct stat info;
+    if (stat(path, &info) != 0) {
+        return false;
+    }
+    return (info.st_mode & S_IFDIR) != 0;
+}
+
+// Функция для создания директорий по переданному пути, если они отсутствуют
+bool createDirectories(const char *rootPath, const char *path) {
+    char *pathCopy = strdup(path);
+    char *dirName = strtok(pathCopy, "/");
+    char *subDirPath = NULL;
+    bool success = true;
+
+    // Добавляем корневой путь к переданному пути
+    char *fullPath = (char *) malloc(strlen(rootPath) + strlen(path) + 2);
+    strcpy(fullPath, rootPath);
+    strcat(fullPath, "/");
+    strcat(fullPath, path);
+
+    while (dirName != NULL) {
+        if (subDirPath == NULL) {
+            subDirPath = strdup(dirName);
+        } else {
+            subDirPath = (char *) realloc(subDirPath, strlen(subDirPath) + strlen(dirName) + 2);
+            strcat(subDirPath, "/");
+            strcat(subDirPath, dirName);
+        }
+
+        // Добавляем корневой путь к текущему пути
+        char *fullSubPath = (char *) malloc(strlen(rootPath) + strlen(subDirPath) + 2);
+        strcpy(fullSubPath, rootPath);
+        strcat(fullSubPath, "/");
+        strcat(fullSubPath, subDirPath);
+
+        if (!directoryExists(fullSubPath)) {
+            if (mkdir(fullSubPath, 0777) != 0) {
+                success = false;
+                free(pathCopy);
+                free(subDirPath);
+                free(fullPath);
+                free(fullSubPath);
+                return success;
+            }
+        }
+
+        free(fullSubPath);
+        dirName = strtok(NULL, "/");
+    }
+
+    free(pathCopy);
+    free(subDirPath);
+    free(fullPath);
+    return success;
+}
+
+char* getFilePath(const char* filename) {
+    char* path = NULL;
+    char* lastSlash = strrchr(filename, '/');
+    if (lastSlash != NULL) {
+        int pathLen = lastSlash - filename + 1;
+        path = (char*) malloc(pathLen + 1);
+        strncpy(path, filename, pathLen);
+        path[pathLen] = '\0';
+    }
+    return path;
+}
+// Функция для разбиения файла на несколько файлов
 void my_split(const char* filename){
-    ///home/bokar/Documents/GitHub/StudyMai/Учеба 6 сем/Операционные системы/ЛР
-    //const char *filename = "/home/bokar/Documents/LR1/output.txt";
+    // Открываем файл для чтения
     FILE *fp = fopen(filename, "r");
+    // Если не удалось открыть файл, выводим сообщение об ошибке и завершаем работу программы
     if (fp == NULL) {
         printf("Ошибка при открытии файла %s\n", filename);
         exit(1);
     }
+    printf("file name = %s\n", filename);
 
+   
+    // Проверяем, что имя файла заканчивается на .txt
     if(!isCorrectNameFile(filename)) {
         printf("Строка не заканчивается на .txt\n");
         printf("Укажите путь к архиву с расширением .txt\n");
         exit(1);
     }
 
+    // Объявляем переменные
     char line[MAX_LINE_LEN];
     char file_data[MAX_LINE_LEN];
     char new_filename[MAX_FILENAME_LEN];
@@ -194,25 +304,43 @@ void my_split(const char* filename){
     int line_num = 0;
     bool isCorrectStartFormat = false;
     bool isCorrectEndFormat = true;
+    char permissionCode[10];
 
+    // Читаем файл построчно
     while (fgets(line, MAX_LINE_LEN, fp) != NULL) {
         line_num++;
 
-        // Если встретили начальный разделитель
+        // Если встретился начальный разделитель
         if (strstr(line, "<<<<") != NULL && isCorrectEndFormat) {
+            // Извлекаем код разрешений из строки
+            char sep[] = "|||";
+            char *permission_start = strstr(line, sep) + 3;
+            char *permission_end = strstr(permission_start, sep);
+            if (permission_end != NULL) {
+                int permission_len = permission_end - permission_start;
+                char permission_str[permission_len + 1];
+                strncpy(permission_str, permission_start, permission_len);
+                permission_str[permission_len] = '\0';
+                int code = atoi(permission_str);
+                sprintf(permissionCode, "%d", code);
+            }
+
             // Извлекаем имя нового файла из строки
             char *filename_start = strstr(line, "</") + 1;
-            char *filename_end = strstr(line, "\n");
+            char *filename_end = permission_start - strlen(sep);
             int filename_len = filename_end - filename_start;
             strncpy(new_filename, filename_start, filename_len);
             new_filename[filename_len] = '\0';
 
+            // Проверяем, что имя файла заканчивается на .txt
             if(!isCorrectNameFile(new_filename)) {
                 printf("Строка не заканчивается на .txt\n");
                 printf("Укажите путь к архиву с расширением .txt\n");
                 exit(1);
+            } else {
+                printf("code %s\n", permissionCode);
+                //fileSetPermissionsCStr(new_filename, *permissionCode);
             }
-
             // Очищаем буфер для нового файла
             memset(file_data, 0, MAX_LINE_LEN);
 
@@ -221,14 +349,22 @@ void my_split(const char* filename){
             // Устанавливаем флаг, что мы находимся внутри файла
             in_file = 1;
         }
-        // Если встретили конечный разделитель
+        // Если встретился конечный разделитель
         else if (strstr(line, ">>>>") != NULL && isCorrectStartFormat) {
             // Создаем новый файл и записываем в него данные
-
             FILE *file_ptr;
+
             printf("%s\n", new_filename);
+            char* path = getFilePath(new_filename);
+            if (path != NULL) {
+                printf("Путь до файла: %s\n", path);
+                createDirectories("", path);
+                free(path);
+            } else {
+                 printf("Файл не содержит пути.\n");
+            }
+
             file_ptr = fopen(new_filename, "w+");
-            
             int wrnum = write_file(file_data, strlen(file_data), file_ptr);
             if (wrnum == strlen(file_data)) {
                 //printf("Запись \"%s\" произошла успешно\n", file_data);
@@ -236,7 +372,6 @@ void my_split(const char* filename){
             else {
                 printf("Данные не были записаны в полном объеме: \"%s\"", file_data);
             }
-            // fwrite возвращает колво успешно записанных символов
             // Сбрасываем флаг, что мы находимся внутри файла
             in_file = 0;
             isCorrectStartFormat = false;
@@ -253,8 +388,10 @@ void my_split(const char* filename){
         }
     }
 
+    // Закрываем файл
     fclose(fp);
 }
+
 
 void archive(){
     char path[LEN];
@@ -262,7 +399,6 @@ void archive(){
     printf("Введите путь к директории архивации: ");
     fgets(path, LEN, stdin);
     path[strcspn(path, "\n")] = 0; // удаляем символ переноса строки
-    //getchar(); // ждем нажатия Enter
     printf("Ваш путь: %s\n", path);
     ///home/bokar/Documents/LR2 LR2
     //const char *path = "/home/bokar/Documents/LR1"; // путь архивации директориии
@@ -286,7 +422,8 @@ void archive(){
 
 }
 int main() {
-
+// /home/bokar/Documents/LR1/
+// /home/bokar/Documents/LR1/output.txt
     int menu;
     printf("1: Заархивируйте директорию, указав путь в программе\n");
     printf("2: Разархивировать, введя путь к файлу архива с помощью клавиатуры\n");
@@ -300,8 +437,6 @@ int main() {
             break;
         case 2:
             printf("Твой выбор разархивировать директорию\n");
-            //const char *path = "/home/bokar/Documents/LR1/output.txt"; // путь разархивации
-    
             printf("Введите путь к файлу архива, например /home/bokar/Documents/LR1/output.txt: ");
             char path[LEN];
     
@@ -309,19 +444,13 @@ int main() {
             path[strcspn(path, "\n")] = 0; // удаляем символ переноса строки
     
             printf("Ваш путь: %s\n", path);
-
             my_split(path);
 
             break;
         default:
-            printf("ERROR! Wrong menu item selected");
+            printf("Ошибка! Неправильно выбранный пункт меню");
             return 0;
     }
-
-    
-
-    
-
 
     return 0;
 }
